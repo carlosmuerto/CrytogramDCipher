15458b1bfd89e43e69e7fd93113f822409422ab6
Carlos A. Vivas N <carlosmuerto@gmail.com>
2017-12-16 19:26:11 -0400

    Base Lista.

--- CrytogramDCipher/Criptograma.cs
+++ CrytogramDCipher/Criptograma.cs
@@ -11,16 +11,19 @@ namespace CrytogramDCipher
 	class Criptograma
 	{
         
-		private Dictionary<Char, Char> _alfP;
-		private Dictionary<Char, Char> _alfC;
+		private Dictionary<Char, Char> _IndexAlfE;
+		private Dictionary<Char, Char> _IndexAlfD;
 
 		private String[] _Palabras;
-		private Dictionary<Int32, Int32> _hzIndex;
+		private Dictionary<Int32, Int32> _HzIndex;
 
-		private Dictionary<Char, Char> AlfP { get => _alfP; set => _alfP = value; }
-		private Dictionary<Char, Char> AlfC { get => _alfC; set => _alfC = value; }
+		private Dictionary<Char, Char> IndexAlfE { get => this._IndexAlfE; set => this._IndexAlfE = value; }
+		private Dictionary<Char, Char> IndexAlfD { get => this._IndexAlfD; set => this._IndexAlfD = value; }
 
-		public String[] Palabras { get => _Palabras; set => _Palabras = value; }
+		private String _AlfP;
+		private String _AlfC;
+
+		public String[] Palabras { get => this._Palabras; set => this._Palabras = value; }
 
 		public BigInteger AlfCode
 		{
@@ -31,13 +34,13 @@ namespace CrytogramDCipher
 
 				ArrayList Alf = new ArrayList();
 				ArrayList Cfr = new ArrayList();
-				foreach (KeyValuePair<Char, Char> Par in this.AlfP)
+				foreach (KeyValuePair<Char, Char> Par in this.IndexAlfE)
 				{
 					Alf.Add(Par.Key);
 					Cfr.Add(Par.Value);
 				}
 
-				foreach (KeyValuePair<Char, Char> Par in this.AlfP)
+				foreach (KeyValuePair<Char, Char> Par in this.IndexAlfE)
 				{
 					Int32 CharIndex = Alf.IndexOf(Cfr[0]);
 					Cfr.RemoveAt(0);
@@ -55,8 +58,9 @@ namespace CrytogramDCipher
 			{
 				// copiar Alfabeto plano en una Arreglo dinamico
 				ArrayList Alf = new ArrayList();
-				Dictionary<Char, Char> AlfTemp = new Dictionary<Char, Char>();
-				foreach (KeyValuePair<Char, Char> par in AlfP)
+				Dictionary<Char, Char> TempIndexAlfP = new Dictionary<Char, Char>();
+				String TempAlfC = "";
+				foreach (KeyValuePair<Char, Char> par in this.IndexAlfE)
 				{
 					Alf.Add(par.Key);
 				}
@@ -65,53 +69,114 @@ namespace CrytogramDCipher
 
 				// divisiones consecutivas con devisores redicido
 				BigInteger Dvdo = value; // Dvdo: Dividendo
-				foreach (KeyValuePair<Char, Char> Par in this.AlfP)
-				{
+				foreach (KeyValuePair<Char, Char> Par in this.IndexAlfE){
 					Dvdo = BigInteger.DivRem(Dvdo, Alf.Count, out BigInteger Rst);
 					Index = Int32.Parse(Rst.ToString());
 
 					//Crear Nuevo Alfabetos
-					AlfTemp.Add(Par.Key, (Char)Alf[Index]);
+					TempIndexAlfP.Add(Par.Key, (Char)Alf[Index]);
 
 					Alf.RemoveAt(Index);
 				}
 
-				foreach (KeyValuePair<Char, Char> par in AlfTemp)
-				{
-					this.AlfP[par.Key] = par.Value;
-					this.AlfC[par.Value] = par.Key;
+				// Actualiza La relacion entre los Alfabetos
+				foreach (KeyValuePair<Char, Char> par in TempIndexAlfP){
+					TempAlfC += par.Value; 
+					this.IndexAlfE[par.Key] = par.Value;
+					this.IndexAlfD[par.Value] = par.Key;
+				}
+
+				//Define el nuevo AlfC que corresponde a los IndexAlf
+				this.AlfC = TempAlfC;
+			}
+		}
+
+		public Dictionary<Int32, Int32> HzIndex { get => this._HzIndex; set => this._HzIndex = value; }
+
+		public String AlfP { get => this._AlfP; private set => this._AlfP = value; }
+		public String AlfC {
+			get => this._AlfC;
+			set {
+				this._AlfC = "";
+				String Temp = value.ToUpper() + this.AlfP; // Concatena
+				foreach (Char ch in Temp){ // Elije la primera concurrecina del las letras 
+				
+					if (Char.IsLetter(ch) &&  !this._AlfC.Contains(ch))
+					{
+						this._AlfC += ch;
+					}
+				}
+
+				// Actualiza La relacion entre los Alfabetos
+				
+
+				for (Int32 i = 0; i< this._AlfP.Length; ++i) {
+					this.IndexAlfE[this._AlfP[i]] = this._AlfC[i];
+					this.IndexAlfD[this._AlfC[i]] = this._AlfP[i];
 				}
 			}
+		}
+
+		public String Cifrar(String TextoPlano)
+		{
+			String TextoCrifrado = "";
+			for (Int32 i = 0; i < TextoPlano.Length; ++i)
+			{
+				if (this.IndexAlfE.TryGetValue(Char.ToUpper(TextoPlano[i]), out Char temp)) {
+					TextoCrifrado += temp;
+				} else {
+					TextoCrifrado += TextoPlano[i];
+				}
+			}
+			return TextoCrifrado;
+		}
+
+		public String Decifrar(String TextoCrifrado)
+		{
+			String TextoPlano = "";
+			for (Int32 i = 0; i < TextoCrifrado.Length; ++i)
+			{
+				if (this.IndexAlfD.TryGetValue(Char.ToUpper(TextoCrifrado[i]), out Char temp)) {
+					TextoPlano += temp;
+				} else {
+					TextoPlano += TextoCrifrado[i];
+				}
+			}
+			return TextoPlano;
+
 		}
 
 		public Criptograma()
         {
 
-			this.AlfP = new Dictionary<Char, Char>() {
+			this.IndexAlfE = new Dictionary<Char, Char>() {
 				{'A','A'},{'B','B'},{'C','C'},{'D','D'},{'E','E'},{'F','F'},
 				{'G','G'},{'H','H'},{'I','I'},{'J','J'},{'K','K'},{'L','L'},
 				{'M','M'},{'N','N'},{'O','O'},{'P','P'},{'Q','Q'},{'R','R'},
 				{'S','S'},{'T','T'},{'U','U'},{'V','V'},{'W','W'},{'X','X'},
 				{'Y','Y'},{'Z','Z'}
 			};
+			this.IndexAlfD = new Dictionary<Char, Char>();
 
-			foreach (KeyValuePair<Char, Char> Par in this.AlfP)
+			this.AlfP = "";
+
+			foreach (KeyValuePair<Char, Char> Par in this.IndexAlfE)
 			{
-				this.AlfC.Add(Par.Value,Par.Key);
+				this.AlfP += Par.Key;
 			}
+			this.AlfC = this.AlfP;
 
 			String[] hzWords = System.IO.File.ReadAllLines(@"105words.txt");
 
 			this.Palabras = (String[]) hzWords.Clone();
 			Array.Sort(this.Palabras);
 
-			this._hzIndex = new Dictionary<Int32, Int32>();
-			Int32 i = 0;
-			foreach (String line in hzWords)
+			this.HzIndex = new Dictionary<Int32, Int32>();
+			
+			for (Int32 i = 0; i < hzWords.Length; ++i)
 			{
-				this._hzIndex.Add(i++, Array.BinarySearch(this.Palabras,line));
+				this.HzIndex.Add(i, Array.BinarySearch(this.Palabras,hzWords[i]));
 			}
-
 		}
     }
 }
--- CrytogramDCipher/Form1.cs
+++ CrytogramDCipher/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Numerics;
 
 namespace CrytogramDCipher
 {
@@ -19,8 +20,59 @@ namespace CrytogramDCipher
         {
 			this.Diccionario = new Criptograma();
             InitializeComponent();
-			this.Diccionario.AlfCode=1000;
-			Console.WriteLine(this.Diccionario.AlfCode);
+
+			this.textBoxAlfP.Text = this.Diccionario.AlfP;
+			this.textBoxAlfC.Text = this.Diccionario.AlfC;
+
+			this.textBoxCodNum.Text = this.Diccionario.AlfCode.ToString();
 		}
-    }
+
+		private void TextBoxAlfC_TextChanged(Object sender, EventArgs e)
+		{
+			this.textBoxCodNum.TextChanged -= new System.EventHandler(this.TextBoxCodNum_TextChanged);
+			this.textBoxAlfC.TextChanged -= new System.EventHandler(this.TextBoxAlfC_TextChanged);
+
+			this.Diccionario.AlfC = this.textBoxAlfC.Text;
+			Int32 stick = this.textBoxAlfC.SelectionStart;
+			// !!! AlfC no es el mismo al que se le untroduce
+			this.textBoxAlfC.Text = this.Diccionario.AlfC;
+			this.textBoxAlfC.SelectionStart = stick;
+
+			this.textBoxCodNum.Text = this.Diccionario.AlfCode.ToString();
+
+			this.textBoxAlfC.TextChanged += new System.EventHandler(this.TextBoxAlfC_TextChanged);
+			this.textBoxCodNum.TextChanged += new System.EventHandler(this.TextBoxCodNum_TextChanged);
+		}
+
+		private void TextBoxCodNum_TextChanged(Object sender, EventArgs e)
+		{
+			this.textBoxCodNum.TextChanged -= new System.EventHandler(this.TextBoxCodNum_TextChanged);
+			this.textBoxAlfC.TextChanged -= new System.EventHandler(this.TextBoxAlfC_TextChanged);
+
+			if (BigInteger.TryParse(this.textBoxCodNum.Text, out BigInteger result)) {
+				this.Diccionario.AlfCode = result;
+
+				this.textBoxAlfC.Text = "";
+				this.textBoxAlfC.Text = this.Diccionario.AlfC;
+			} else {
+				if (this.textBoxCodNum.Text == "") {
+					this.Diccionario.AlfCode = 0;
+				}
+				this.textBoxAlfC.Text = this.Diccionario.AlfC;
+			}
+
+			this.textBoxAlfC.TextChanged += new System.EventHandler(this.TextBoxAlfC_TextChanged);
+			this.textBoxCodNum.TextChanged += new System.EventHandler(this.TextBoxCodNum_TextChanged);
+		}
+
+		private void ButtonCifrado_Click(Object sender, EventArgs e)
+		{
+			this.textBoxOut.Text = this.Diccionario.Cifrar(this.textBoxIn.Text);
+		}
+
+		private void ButtonDecifrado_Click(Object sender, EventArgs e)
+		{
+			this.textBoxOut.Text = this.Diccionario.Decifrar(this.textBoxIn.Text);
+		}
+	}
 }
